45280a7caba13bac464f776eeb3d34b625bfa91c
t-taishi <t-taishi@jpx.co.jp>
2021-10-06 00:13:52 +0900

    fix ts-node-dev

---
+++ src/boolean.ts
@@ -0,0 +1,10 @@
+// exportすることでモジュール化出来るので、グローバル空間で使ってるものも使えるようになる
+export {};
+
+let name = 'Typescript';
+
+let isFinished: boolean = true;
+isFinished = false;
+// isFinished = 1;
+console.log({ isFinished });
+
